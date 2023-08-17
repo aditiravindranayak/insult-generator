0297b416f9e44914635b08d55de2e0c69bc5ed6d
aditiravindranayak <109815646+aditiravindranayak@users.noreply.github.com>
2023-08-18 02:23:49 +0530

    code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+from random import randint
+def get_random_word(words):
+return words[randint(0, len(words) - 1)]
+opinion = ['stupid', 'weird', 'boring', 'witless']
+size = ['huge', 'big', 'little', 'tiny']
+quality = ['scabby', 'spotty', 'saggy', 'ugly']
+age = ['old', 'immature', 'childish', 'decrepit']
+shape = ['fat', 'skinny', 'clunking', 'lanky']
+noun = ['idiot', 'halfwit', 'coward', 'crybaby']
+sentence = 'You {}, {}, {}, {}, {} {}'.format(
+get_random_word(opinion),
+get_random_word(size),
+get_random_word(quality),
+get_random_word(age),
+get_random_word(shape),
+get_random_word(noun)
+)
+print(sentence)
